7ab2cfbdfef14426f7414ec7632fa62ac2e7b0f1
Sjoerd de Valk <sjoerddevalk@outlook.com>
2019-05-20 00:38:24 +0200

    initial release

---
+++ ConvertOneNote2MarkDown.ps1
@@ -0,0 +1,222 @@
+Function Remove-InvalidFileNameChars {
+  param(
+    [Parameter(Mandatory = $true,
+      Position = 0,
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true)]
+    [String]$Name
+  )
+  $newName = $Name.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+  return ($newName.Replace(" ", "_"))
+}
+
+# ask for the Notes root path
+$notesdestpath = Read-Host -Prompt "Enter the (preferably empty!) folder path (without trailing backslash!) that will contain your resulting Notes structure. ex. 'c:\temp\notes'"
+
+if (Test-Path -Path $notesdestpath) {
+  # open OneNote hierarchy
+  $OneNote = New-Object -ComObject OneNote.Application
+  [xml]$Hierarchy = ""
+  $totalerr = ""
+  $OneNote.GetHierarchy("", [Microsoft.Office.InterOp.OneNote.HierarchyScope]::hsPages, [ref]$Hierarchy)
+
+  foreach ($notebook in $Hierarchy.Notebooks.Notebook) {
+    #if ($notebook.Name -eq "KW1C Portaal Notitieblok" -or $notebook.Name -eq "CHDR - CoCo Notebook") {
+      " "
+      $notebook.Name
+      $notebookFileName = "$($notebook.Name)" | Remove-InvalidFileNameChars
+      New-Item -Path "$($notesdestpath)\" -Name "$($notebookFileName)" -ItemType "directory" -ErrorAction SilentlyContinue
+      "=============="
+
+      foreach ($sectiongroup in $notebook.SectionGroup) {
+        if ($sectiongroup.isRecycleBin -ne 'true') {
+          "## " + $sectiongroup.Name
+        }
+      }
+      foreach ($section in $notebook.Section) {
+        #if ($section.Name -eq "IBN - Aanpassen HR Forms en Workflow") {
+          "--------------"
+          "### " + $section.Name
+          $sectionFileName = "$($section.Name)" | Remove-InvalidFileNameChars
+          New-Item -Path "$($notesdestpath)\$($notebookFileName)" -Name "$($sectionFileName)" -ItemType "directory" -ErrorAction SilentlyContinue
+          [int]$previouspagelevel = 1
+          [string]$previouspagenamelevel1 = ""
+          [string]$previouspagenamelevel2 = ""
+          [string]$pageprefix = ""
+          foreach ($page in $section.Page) {
+            "#### " + $page.name
+            #if ($page.name -eq "Documentatie") {
+              # set page variables
+              $recurrence = 1
+              $pagelevel = $page.pagelevel
+              $pagelevel = $pagelevel -as [int]
+              $pageid = ""
+              $pageid = $page.ID
+              $pagename = ""
+              $pagename = $page.name | Remove-InvalidFileNameChars
+              $fullexportdirpath = ""
+              $fullexportdirpath = "$($notesdestpath)\$($notebookFileName)\$($sectionFileName)"
+              $fullexportpathwithoutextension = ""
+              $fullexportpathwithoutextension = "$($fullexportdirpath)\$($pagename)"
+              $fullexportpath = ""
+              $fullexportpath = "$($fullexportpathwithoutextension).docx"
+
+              # make sure there is no existing Word file
+              if ([System.IO.File]::Exists($fullexportpath)) {
+                try {
+                  Remove-Item -path $fullexportpath -Force -ErrorAction SilentlyContinue
+                }
+                catch {
+                  Write-Host "Error removing intermediary '$($page.name)' docx file: $($Error[0].ToString())" -ForegroundColor Red
+                  $totalerr += "Error removing intermediary '$($page.name)' docx file: $($Error[0].ToString())`r`n"
+                }
+              }
+
+              # in case multiple pages with the same name exist in a section, postfix the filename
+              if ([System.IO.File]::Exists("$($fullexportpathwithoutextension).md")) {
+                $pagename = "$($pagename)_$recurrence"
+                $recurrence++
+              }
+
+              # determine right name prefix based on pagelevel
+              if ($pagelevel -eq 1) {
+                $pageprefix = ""
+                $previouspagenamelevel1 = $pagename
+                $previouspagenamelevel2 = ""
+                $previouspagelevel = 1
+              }
+              elseif ($pagelevel -gt $previouspagelevel) {
+                if ($pagelevel -eq 2) {
+                  $pageprefix = "$($previouspagenamelevel1)"
+                  $previouspagenamelevel2 = $pagename
+                  $previouspagelevel = 2
+                }
+                if ($pagelevel -eq 3) {
+                  $pageprefix = "$($previouspagenamelevel1)_$($previouspagenamelevel2)"
+                  $previouspagelevel = 3
+                }
+              }
+              elseif ($pagelevel -eq $previouspagelevel -and $pagelevel -ne 1) {
+                if ($pagelevel -eq 2) {
+                  $pageprefix = "$($previouspagenamelevel1)"
+                  $previouspagenamelevel2 = $pagename
+                }
+                if ($pagelevel -eq 3) {
+                  $pageprefix = "$($previouspagenamelevel1)_$($previouspagenamelevel2)"
+                }
+              }
+              elseif ($pagelevel -lt $previouspagelevel -and $pagelevel -ne 1) {
+                if ($pagelevel -eq 2) {
+                  $pageprefix = "$($previouspagenamelevel1)"
+                  $previouspagenamelevel2 = $pagename
+                  $previouspagelevel = 2
+                }
+              }
+              if ($pageprefix) {
+                $pagename = "$($pageprefix)_$($pagename)"
+              }
+              $fullexportpathwithoutextension = "$($fullexportdirpath)\$($pagename)"
+
+              # publish OneNote page to Word
+              try {
+                $OneNote.Publish($pageid, $fullexportpath, "pfWord", "")
+              }
+              catch {
+                Write-Host "Error while publishing file '$($page.name)' to docx: $($Error[0].ToString())" -ForegroundColor Red
+                $totalerr += "Error while publishing file '$($page.name)' to docx: $($Error[0].ToString())`r`n"
+              }
+
+              # convert Word to Markdown
+              # https://gist.github.com/heardk/ded40b72056cee33abb18f3724e0a580
+              try {
+                pandoc.exe -f docx -t gfm -i $fullexportpath -o "$($fullexportpathwithoutextension).md" --wrap=none --atx-headers --extract-media="$($fullexportdirpath)"
+              }
+              catch {
+                Write-Host "Error while converting file '$($page.name)' to md: $($Error[0].ToString())" -ForegroundColor Red
+                $totalerr += "Error while converting file '$($page.name)' to md: $($Error[0].ToString())`r`n"
+              }
+
+              # export inserted file objects
+              [xml]$pagexml = ""
+              $OneNote.GetPageContent($pageid, [ref]$pagexml, 7)
+
+              $pageinsertedfiles = $pagexml.Page.Outline.OEChildren.OE | Where-Object { $_.InsertedFile }
+              foreach ($pageinsertedfile in $pageinsertedfiles) {
+                $destfilename = ""
+                try {
+                  $destfilename = $pageinsertedfile.InsertedFile.preferredName | Remove-InvalidFileNameChars
+                  Copy-Item -Path "$($pageinsertedfile.InsertedFile.pathCache)" -Destination "$($fullexportdirpath)\$($destfilename)" -Force
+                }
+                catch {
+                  Write-Host "Error while copying file object '$($pageinsertedfile.InsertedFile.preferredName)' for page '$($page.name)': $($Error[0].ToString())" -ForegroundColor Red
+                  $totalerr += "Error while copying file object '$($pageinsertedfile.InsertedFile.preferredName)' for page '$($page.name)': $($Error[0].ToString())`r`n"
+                }
+                # Change MD file Object Name References
+                try {
+                  ((Get-Content -path "$($fullexportpathwithoutextension).md" -Raw).Replace("$($pageinsertedfile.InsertedFile.preferredName)", "[$($destfilename)](./$($destfilename))")) | Set-Content -Path "$($fullexportpathwithoutextension).md"
+                }
+                catch {
+                  Write-Host "Error while renaming file object name references to '$($pageinsertedfile.InsertedFile.preferredName)' for file '$($page.name)': $($Error[0].ToString())" -ForegroundColor Red
+                  $totalerr += "Error while renaming file object name references to '$($pageinsertedfile.InsertedFile.preferredName)' for file '$($page.name)': $($Error[0].ToString())`r`n"
+                }
+              }
+
+              # rename images
+              $timeStamp = (Get-Date -Format o).ToString()
+              $timeStamp = $timeStamp.replace(':', '')
+              $re = [regex]"\d{4}-\d{2}-\d{2}T"
+              $images = Get-ChildItem -Path "$($fullexportdirpath)/media" -Include "*.png", "*.gif", "*.jpg", "*.jpeg" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -notmatch $re }
+              foreach ($image in $images) {
+                $newimageName = "$($image.BaseName)_$($timeStamp)$($image.Extension)"
+                # Rename Image
+                try {
+                  Rename-Item -Path "$($image.FullName)" -NewName $newimageName -ErrorAction SilentlyContinue
+                }
+                catch {
+                  Write-Host "Error while renaming image '$($image.FullName)' for page '$($page.name)': $($Error[0].ToString())" -ForegroundColor Red
+                  $totalerr += "Error while renaming image '$($image.FullName)' for page '$($page.name)': $($Error[0].ToString())`r`n"
+                }
+                # Change MD file Image Name References
+                try {
+                  ((Get-Content -path "$($fullexportpathwithoutextension).md" -Raw).Replace("$($image.Name)", "$($newimageName)")) | Set-Content -Path "$($fullexportpathwithoutextension).md"
+                }
+                catch {
+                  Write-Host "Error while renaming image file name references to '$($image.Name)' for file '$($page.name)': $($Error[0].ToString())" -ForegroundColor Red
+                  $totalerr += "Error while renaming image file name references to '$($image.Name)' for file '$($page.name)': $($Error[0].ToString())`r`n"
+                }
+              }
+
+              # change MD file Image Path References
+              try {
+                # Change MD file Image Path References in Markdown
+                ((Get-Content -path "$($fullexportpathwithoutextension).md" -Raw).Replace("$($fullexportdirpath.Replace("\","\\"))/", "")) | Set-Content -Path "$($fullexportpathwithoutextension).md"
+                # Change MD file Image Path References in HTML
+                ((Get-Content -path "$($fullexportpathwithoutextension).md" -Raw).Replace("$($fullexportdirpath)/", "")) | Set-Content -Path "$($fullexportpathwithoutextension).md"
+              }
+              catch {
+                Write-Host "Error while renaming image file path references for file '$($page.name)': $($Error[0].ToString())" -ForegroundColor Red
+                $totalerr += "Error while renaming image file path references for file '$($page.name)': $($Error[0].ToString())`r`n"
+              }
+
+              # Cleanup Word files
+              try {
+                Remove-Item -path "$fullexportpath" -Force -ErrorAction SilentlyContinue
+              }
+              catch {
+                Write-Host "Error removing intermediary '$($page.name)' docx file: $($Error[0].ToString())" -ForegroundColor Red
+                $totalerr += "Error removing intermediary '$($page.name)' docx file: $($Error[0].ToString())`r`n"
+              }
+            #}
+          }
+        #}
+      }
+    #}
+  }
+  # release OneNote hierarchy
+  [System.Runtime.Interopservices.Marshal]::ReleaseComObject($OneNote)
+  Remove-Variable OneNote
+  $totalerr
+}
+else {
+  Write-Host "This path is NOT valid"
+}
--- README.md
+++ README.md
@@ -1,1 +1,111 @@
-# ConvertOneNote2MarkDown
+---
+title:  'Convert OneNote to MarkDown'
+author:
+- Sjoerd de Valk, Motion10
+date: 2019-05-19 22:35:00
+keywords: [migration, tooling]
+abstract: |
+  This document is about converting you OneNote data to Markdown.
+---
+# Convert OneNote to MarkDown
+
+[![Github All Releases](https://img.shields.io/github/downloads/SjoerdV/ConvertOneNote2MarkDown/total.svg)](https://github.com/SjoerdV/ConvertOneNote2MarkDown/releases)
+
+## Summary
+
+!!! question Ready to make the step to Markdown and saying farewell to your OneNote, EverNote or whatever proprietary note taking tool you are using? Nothing beats clear text, right? Read on!
+
+The powershell script 'ConvertOneNote2MarkDown.ps1' will utilize the OneNote Object Model on your workstation to convert all OneNote pages to Word documents and then utilizes PanDoc to convert the Word documents to Markdown (.md) format. It will also:
+
+* Create a **folder structure** for your Notebooks and Sections
+* Append **prefixes to page filenames** if they were indented beneath other pages (so called 'page levels')
+* Extract all **Images** to the '/media' folder of each section and fix references in the resulting .md files
+* Extract all **File Objects** to the same folder as where the page is in and fix references in the resulting .md files
+* Cleanup intermediate Word files
+
+## Known Issues
+
+1. Password protected sections should be unlocked before continuing, the Object Model does not have access to them if you don't
+1. Section Groups are listed but do not contain actual data, so these are ignored
+1. You should start by 'flattening' all pen/hand written elements in your onennote pages. Because OneNote does not have this function you will have to take screenshots of your pages with pen/hand written notes and paste the resulting image and then remove the scriblings. If you are a heavy 'pen' user this is a very cumbersome. **If you have an automated solution for this, please let me know**
+1. While running the conversion OneNote will be unusable and it is recommended to 'walk away' and have some coffee as the Object Model might be interrupted if you do anything else.
+1. Linked file object in .md files are clickable in VSCode, but do not open in their associated program, you will have to open the files directly from the file system.
+1. Anything I did not catch... please submit an issue.
+
+## Requirements
+
+* Windows >= 10
+
+  * I have only tested this on Windows...
+
+* Microsoft OneNote >= 2016
+
+* PanDoc >= 2.7.2
+
+  * TIP: Use [Chocolatey](https://chocolatey.org/docs/installation#install-with-powershellexe) to install this on Windows, this will also set the right path (environment) statements
+
+## Installation
+
+Clone this repository to acquire the powershell script.
+
+## Usage
+
+1. Start the OneNote application. All notebooks currently loaded in OneNote will be converted
+1. Open a PowerShell terminal and navigate to the folder containing the script and run it:
+
+    ```powershell
+    '.\ConvertOneNote2MarkDown.ps1'
+    ```
+
+1. It will ask you for the path to store the markdown folder structure. Please use an empty folder.
+1. Sit back and wait until the process completes
+
+## Results
+
+The script will log any errors encountered at the end of its run, so please review, fix and run again if needed.
+If you are satisfied check the results with a markdown editor like VSCode. All images should popup just right in the Preview Pane for Markdown files.
+
+## Recommendations
+
+1. I would like to recommend this repository [VSCodeNotebook](https://github.com/aviaryan/VSCodeNotebook) to host your resulting Markdown Notes folder structure. This solution supports encrypting sensitive (markdown) files and works quite nicely.
+1. While working with markdown in VSCode these are the extensions I like using:
+
+```powershell
+    .\code `
+    --install-extension davidanson.vscode-markdownlint `
+    --install-extension ms-vscode.powershell-preview `
+    --install-extension jebbs.markdown-extended `
+    --install-extension telesoho.vscode-markdown-paste-image `
+    --install-extension redhat.vscode-yaml `
+    --install-extension vscode-icons-team.vscode-icons `
+    --install-extension ms-vsts.team
+```
+
+> NOTE: The bottom three are not really markdown related but are quite obvious.
+
+## Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+### [Unreleased]
+
+### [1.0.0] - 2019-05-19
+
+#### Added
+
+* Initial Release
+
+#### Changed
+
+* Nothing
+
+#### Removed
+
+* Nothing
+
+## Credits
+
+* Avi Aryan for the awesome [VSCodeNotebook](https://github.com/aviaryan/VSCodeNotebook) port
